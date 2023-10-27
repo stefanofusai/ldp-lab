0f9d0eac2efed58938b306596808ef8d0c963bef
Stefano Fusai <stefanofusai@gmail.com>
2023-10-27 15:57:49 +0200

    Working on beginner exercises

---
+++ ocaml/beginner.ml
@@ -0,0 +1,157 @@
+(* https://ocaml.org/exercises?difficulty_level=beginner *)
+
+(* Tail of a List
+   Write a function last : 'a list -> 'a option that returns the last element of a list *)
+let rec last = function
+  | [] -> None
+  | [ x ] -> Some x
+  | _ :: t -> last t;;
+
+(* Last Two Elements of a List
+   Find the last but one (last and penultimate) elements of a list.*)
+let rec last_two = function
+  | [] | [_] -> None
+  | [x; y] -> Some (x, y)
+  | _ :: t -> last_two t;;
+
+(* N'th Element of a List
+   Find the N'th element of a list.
+   Remark: OCaml has List.nth which numbers elements from 0 and raises an exception if the index is out of bounds. *)
+let rec nth_element_of_list lst n = match lst with
+  | [] -> None
+  | h :: t -> if n == 0 then Some h else nth_element_of_list t (n - 1)
+
+(* Length of a List
+   Find the number of elements of a list.
+   OCaml standard library has List.length but we ask that you reimplement it. Bonus for a tail recursive solution. *)
+let rec length = function
+  | [] -> 0
+  | _ :: t -> 1 + length t
+
+(* Reverse a List
+   Reverse a list.
+   OCaml standard library has List.rev but we ask that you reimplement it. *)
+let rev lst =
+  let rec aux acc = function
+    | [] -> acc
+    | h :: t -> aux (h :: acc) t
+  in aux [] lst
+
+(* Palindrome
+   Find out whether a list is a palindrome.
+   Hint: A palindrome is its own reverse. *)
+let is_palindrome lst =
+  lst = rev lst
+
+(* Run-Length Encoding
+   If you need so, refresh your memory about [run-length encoding](https://en.wikipedia.org/wiki/Run-length_encoding). *)
+let encode lst =
+  let rec aux count acc = function
+    | [] -> []
+    | [x] -> (count + 1, x) :: acc
+    | a :: (b :: _ as t) ->
+      if a = b then aux (count + 1) acc t
+      else aux 0 ((count + 1, a) :: acc) t in
+  List.rev (aux 0 [] lst)
+
+(* Modified Run-Length Encoding
+   Modify the result of the previous problem in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+   Since OCaml lists are homogeneous, one needs to define a type to hold both single elements and sub-lists. *)
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+
+let encode lst =
+  let rec aux count acc = function
+    | [] -> []
+    | [x] ->
+      let elem =
+        if count = 0 then One(x)
+        else Many(count + 1, x)
+      in elem :: acc
+    | a :: (b :: _ as t) ->
+      if a = b then aux (count + 1) acc t
+      else
+        let elem =
+          if count = 0 then One(a)
+          else Many(count + 1, a)
+        in aux 0 (elem :: acc) t
+      in List.rev (aux 0 [] lst)
+
+(* Duplicate the Elements of a List
+   Duplicate the elements of a list. *)
+let duplicate lst =
+  let rec aux acc = function
+    | [] -> acc
+    | h :: t -> aux (h :: h :: acc) t
+in List.rev (aux [] lst)
+
+(* Better implementation *)
+let rec duplicate_btr = function
+  | [] -> []
+  | h :: t -> h :: h :: duplicate_btr t
+
+(* Split a List Into Two Parts; The Length of the First Part Is Given
+   Split a list into two parts; the length of the first part is given.
+   If the length of the first part is longer than the entire list, then the first part is the list and the second part is empty. *)
+let split lst i =
+  let rec aux i acc = function
+    | [] -> (List.rev acc, [])
+    | h :: t as l ->
+        if i = 0 then (List.rev acc, l)
+        else aux (i - 1) (h :: acc) t
+  in aux i [] lst
+
+(* Remove the K'th Element From a List
+   Remove the K'th element from a list.
+   The first element of the list is numbered 0, the second 1,...*)
+let rec remove_at i = function
+  | [] -> []
+  | h :: t -> if i = 0 then t else h :: remove_at (i - 1) t
+
+(* Insert an Element at a Given Position Into a List
+   Start counting list elements with 0. If the position is larger or equal to the length of the list, insert the element at the end. (The behavior is unspecified if the position is negative.) *)
+let rec insert_at x i = function
+  | [] -> []
+  | h :: t ->
+    if i - 1 >= List.length t then t @ [x]
+    else
+      if i = 0 then x :: h :: t
+      else h :: insert_at x (i - 1) t
+
+let rec insert_at_btr x i = function
+  | [] -> [x]
+  | h :: t ->
+    if i = 0 then x :: h :: t
+    else h :: insert_at x (i - 1) t
+
+(* Create a List Containing All Integers Within a Given Range
+   If first argument is greater than second, produce a list in decreasing order. *)
+let range x y =
+  let rec aux n i =
+    if i > 0 then n :: aux (n + 1) (i - 1)
+    else [n]
+  in if x <= y then aux x (y - x) else List.rev @@ aux y (x - y)
+
+(* Lotto: Draw N Different Random Numbers From the Set 1..M
+   Draw N different random numbers from the set 1..M.
+   The selected numbers shall be returned in a list. *)
+let rec lotto_select n b =
+  if n > 0 then Random.int b + 1 :: lotto_select (n - 1) b
+  else []
+
+(* Generate a Random Permutation of the Elements of a List
+  Generate a random permutation of the elements of a list. *)
+(* TODO *)
+
+(* Determine Whether Two Positive Integer Numbers Are Coprime
+  Determine whether two positive integer numbers are coprime.
+  Two numbers are coprime if their greatest common divisor equals 1. *)
+let coprime x y = 
+  let rec gcd x y =
+    if x = 0 then y
+    else
+      if y = 0 then x
+      else gcd y (x mod y)
+  in (gcd x y) = 1
+
