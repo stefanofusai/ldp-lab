4085e3e7949d726dbf04d3e9533afb31b05c1d61
Stefano Fusai <stefanofusai@gmail.com>
2023-11-28 18:00:43 +0100

    Improve eval_op

--- erlang/es2.erl
+++ erlang/es2.erl
@@ -1,20 +1,16 @@
 -module(es2).
 -export([eval_exp/1]).
 
-eval_op(divv) -> fun(X, Y) -> X / Y end;
-eval_op(minus) -> fun(X, Y) -> X - Y end;
-eval_op(plus) -> fun(X, Y) -> X + Y end;
-eval_op(prod) -> fun(X, Y) -> X * Y end.
-
-exec_exp(Op, X, Y) ->
-    Fun = eval_op(Op),
-    Fun(X, Y).
+eval_op(divv, X, Y) -> X / Y;
+eval_op(minus, X, Y) -> X - Y;
+eval_op(plus, X, Y) -> X + Y;
+eval_op(prod, X, Y) -> X * Y.
 
 eval_exp({Op, {num, X}, {num, Y}}) ->
-    exec_exp(Op, X, Y);
+    eval_op(Op, X, Y);
 eval_exp({Op, {num, X}, Exp}) ->
-    exec_exp(Op, X, eval_exp(Exp));
+    eval_op(Op, X, eval_exp(Exp));
 eval_exp({Op, Exp, {num, Y}}) ->
-    exec_exp(Op, eval_exp(Exp), Y);
+    eval_op(Op, eval_exp(Exp), Y);
 eval_exp({Op, ExpX, ExpY}) ->
-    exec_exp(Op, eval_exp(ExpX), eval_exp(ExpY)).
+    eval_op(Op, eval_exp(ExpX), eval_exp(ExpY)).
